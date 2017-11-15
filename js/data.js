4735aa6efc57deb2e0c2f53f934f4634c3cf8725
shine1594 <panamera1594@gmail.com>
2017-11-16 00:55:48 +0900

    [FIX] writed one object to one line of code

--- js/data.js
+++ js/data.js
@@ -1,72 +1,24 @@
 (function(window) {
   var moneyList = [
-    {
-      unit: 10,
-      count: 5
-    },
-    {
-      unit: 50,
-      count: 3
-    },
-    {
-      unit: 100,
-      count: 5
-    },
-    {
-      unit: 500,
-      count: 3
-    },
-    {
-      unit: 1000,
-      count: 5
-    },
-    {
-      unit: 5000,
-      count: 3
-    },
-    {
-      unit: 10000,
-      count: 5
-    }
+    { unit: 10, count: 5 },
+    { unit: 50, count: 3 },
+    { unit: 100, count: 5 },
+    { unit: 500, count: 3 },
+    { unit: 1000, count: 5 },
+    { unit: 5000, count: 3 },
+    { unit: 10000, count: 5 }
   ];
 
   var drinks = [
-    {
-      name: "콜라",
-      price: 500
-    },
-    {
-      name: "사이다",
-      price: 1000
-    },
-    {
-      name: "환타파인애플맛",
-      price: 400
-    },
-    {
-      name: "환타포토맛",
-      price: 400
-    },
-    {
-      name: "레몬에이드",
-      price: 500
-    },
-    {
-      name: "봉봉",
-      price: 1200
-    },
-    {
-      name: "코코아주스",
-      price: 1000
-    },
-    {
-      name: "콜라제로",
-      price: 1000
-    },
-    {
-      name: "파워에이드",
-      price: 400
-    }
+    { name: "콜라", price: 500 },
+    { name: "사이다", price: 1000 },
+    { name: "환타파인애플맛", price: 400 },
+    { name: "환타포토맛", price: 400 },
+    { name: "레몬에이드", price: 500 },
+    { name: "봉봉", price: 1200},
+    { name: "코코아주스", price: 1000 },
+    { name: "콜라제로", price: 1000 },
+    { name: "파워에이드", price: 400 }
   ];
 
   window.originalData = {};
