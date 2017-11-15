606370524d58d8c9573d177e95c066fd62320cf9
shine1594 <panamera1594@gmail.com>
2017-11-15 14:56:44 +0900

    [IMP] data that can be changed has been separated

--- js/controller.js
+++ js/controller.js
@@ -7,14 +7,6 @@
             this.model = model;
             this.view = view;
 
-            model.wallet.putMoney(10, 5);
-            model.wallet.putMoney(50, 3);
-            model.wallet.putMoney(100, 5);
-            model.wallet.putMoney(500, 3);
-            model.wallet.putMoney(1000, 5);
-            model.wallet.putMoney(5000, 3);
-            model.wallet.putMoney(10000, 5);
-
             view.init(model);
             view.wallet.bind('loseMoney', this.spendMoney.bind(this));
         },
--- js/model.js
+++ js/model.js
@@ -1,46 +1,11 @@
 (function(window) {
-    var wallet = [
-        {
-            unit: 10,
-            count: 0
-        },
-        {
-            unit: 50,
-            count: 0
-        },
-        {
-            unit: 100,
-            count: 0
-        },
-        {
-            unit: 500,
-            count: 0
-        },
-        {
-            unit: 1000,
-            count: 0
-        },
-        {
-            unit: 5000,
-            count: 0
-        },
-        {
-            unit: 10000,
-            count: 0
-        }
-    ];
+    var wallet = originalData.wallet; //Array
 
     wallet.total = 0;
-
-    wallet.putMoney = function (unit, count) {
-        unit = parseInt(unit);
-        count = parseInt(count);
-
-        var item = this.findMoney(unit);
-
-        item.count += count;
-        this.total += unit * count;
-    }
+    originalData.wallet.forEach(function(item) {
+        wallet.total += item.unit * item.count;
+    });
+    
 
     wallet.loseMoney = function(unit, count) {
         unit = parseInt(unit);
@@ -71,6 +36,7 @@
 
     var machine = {
         money: 0,
+        drinks: originalData.drinks,
         putMoney: function(money) {
             this.money += money;
         },
@@ -82,7 +48,7 @@
     var model = {
         wallet: wallet,
         machine: machine
-    }
+    };
 
     window.vm = window.vm || {};
     window.vm.model = model;
