493392b8991a4640fc015a1400c25352166dc069
shine1594 <panamera1594@gmail.com>
2017-11-17 01:55:38 +0900

    [IMP] cleaned up view.js

--- js/view.js
+++ js/view.js
@@ -1,52 +1,38 @@
 (function (window) {
-    var wallet = {
-        money: document.querySelector('.wallet .money'),
-        moneyList: document.querySelectorAll('.wallet .money > li'),
+    const wallet = {
+        moneyContainer: document.querySelector('.wallet .money'),
+        moneyItems: document.querySelectorAll('.wallet .money > li'),
         moneyUnits: document.querySelectorAll('.wallet .money > li .unit'),
         moneyCounts: document.querySelectorAll('.wallet .money > li .count'),
         totalMoney: document.querySelector('.wallet .total'),
         openToggle: document.querySelector('.wallet .wrap-wallet-icon .icon'),
         bind: function(event, handler) {
-            switch (event) {
-                case 'toggleWallet':
-                    this.openToggle.addEventListener('click', function(evt) {
-                        console.log(this.money);
-                        console.log(this.money.classList);
-                        if (this.money.classList.contains('hide')) {
-                            this.money.classList.remove('hide');
-                        } else {
-                            this.money.classList.add('hide');
-                        }
+            if (event === 'toggleWallet') {
+                this.openToggle.addEventListener('click', function() {
+                    if (this.moneyContainer.classList.contains('hide')) {
+                        this.moneyContainer.classList.remove('hide');
+                    } else {
+                        this.moneyContainer.classList.add('hide');
+                    }
+                }.bind(this));
+            } else if (event === 'loseMoney') {
+                this.moneyUnits.forEach(function(btn, index) {
+                    btn.addEventListener('click', function() {
+                        handler(this.moneyItems[index].dataset.unit);
                     }.bind(this));
-                break;
-                case 'loseMoney':
-                    this.moneyUnits.forEach(function(btn, index) {
-                        btn.addEventListener('click', function() {
-                            var unit = this.moneyList[index].dataset.unit;
-                            handler(unit);
-                        }.bind(this));
-                    }.bind(this));
-                break;
+                }.bind(this));
             }
         },
-        render: function(command, params) {
-            var viewCommands = {
-                updateMoney: function() {
-                    var item = null;
+        renderMoney: function(params) {
+            this.moneyItems.findIndex = Array.prototype.findIndex.bind(this.moneyItems);
 
-                    for (var i = 0; i < this.moneyList.length; i++) {
-                        if (this.moneyList[i].dataset.unit == params.unit) {
-                            item = this.moneyCounts[i];
-                            break;
-                        }
-                    }
+            const index = this.moneyItems.findIndex(item => {
+                return item.dataset.unit == params.unit;
+            });
 
-                    item.textContent = params.count + '개';
-                    this.totalMoney.textContent = params.totalMoney + '원';
-                }.bind(this)
-            };
-
-            viewCommands[command]();
+            const item = this.moneyCounts[index];
+            item.textContent = params.count + '개';
+            this.totalMoney.textContent = params.totalMoney + '원';
         },
         init: function(model) {
             model.moneyList.forEach(function(item) {
@@ -54,62 +40,50 @@
                 var count = model.getCountOfUnit(unit);
                 var totalMoney = model.getTotalMoney();
 
-                this.render('updateMoney', {
+                this.renderMoney({
                     unit: unit,
                     count: count,
                     totalMoney: totalMoney
                 });
-
             }.bind(this));
         }
     }
 
-    var machine = {
+    const machine = {
         itemContainer: document.querySelector('.machine .items'),
         items: null,
         inputBox: document.querySelector('.machine .input'),
         messageBox: document.querySelector('.machine .message'),
         numberButtons: document.querySelectorAll('.machine .buttons button'),
         bind: function(event, handler) {
-            switch (event) {
-                case 'inputItemId':
-                    this.numberButtons.forEach(function(btn) {
-                        btn.addEventListener('click', function() {
-                            var num = btn.dataset.num;
-                            handler(num);
-                        });
+            if (event === 'inputItemId') {
+                this.numberButtons.forEach(function(btn) {
+                    btn.addEventListener('click', function() {
+                        handler(btn.dataset.num);
                     });
-                break;
+                });
             }
         },
-        render: function(command, params) {
-            var viewCommands = {
-                updateMoney: function() {
-                    this.inputBox.textContent = params.money + '원';
-                }.bind(this),
+        renderMessage: function(params) {
+            this.messageBox.innerHTML += params.message + '<br/>';
+        },
+        renderMoney: function(params) {
+            this.inputBox.textContent = params.money + '원';
+        },
+        renderPurchasableItems: function(params) {
+            this.items.forEach(function(item, index) {
+                if (!params.purchasableFlags[index]) {
+                    item.classList.remove('purchasable');
+                    return;
+                };
 
-                updatePurchasableItems: function() {
-                    this.items.forEach(function(item, index) {
-                        if (!params.isPurchasable[index]) {
-                            item.classList.remove('purchasable');
-                            return;
-                        };
-
-                        if (!item.classList.contains('purchasable')) {
-                            item.classList.add('purchasable');
-                        }
-                    })
-                }.bind(this),
-
-                displayMessage: function() {
-                    this.messageBox.innerHTML += params.message + '<br/>';
-                }.bind(this)
-            };
-
-            viewCommands[command]();
+                if (!item.classList.contains('purchasable')) {
+                    item.classList.add('purchasable');
+                }
+            });
         },
         init: function(model) {
-            var drinkItemTemplate = '<li class="col-3">' +
+            var itemTemplate = '<li class="col-3">' +
               '<div class="item">' +
               '<p class="name">{{name}}</p>' +
               '<span class="id">{{index}}</span>' +
@@ -118,7 +92,7 @@
             '</li>';
 
             model.items.forEach(function(item, index) {
-                this.itemContainer.innerHTML += drinkItemTemplate
+                this.itemContainer.innerHTML += itemTemplate
                     .replace('{{name}}', item.name)
                     .replace('{{index}}', (index + 1) + '. ')
                     .replace('{{price}}', item.price);
@@ -128,7 +102,7 @@
         }
     }
 
-    var view = {
+    const view = {
         wallet: wallet,
         machine: machine,
         init: function(model) {
