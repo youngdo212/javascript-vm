d1557de489d6b8d89276be790c79719630fc4876
wfe-luigi <kdh3979@naver.com>
2017-11-30 14:28:29 +0900

    [Modify] DOM value -> object elements

--- script.js
+++ script.js
@@ -9,15 +9,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
         });
     })();
 
+    var elements = { //전역으로 사용하지만 뭔가 코드들이 길어지는 느낌
+        money_button : document.querySelector(".money_button"),
+        money_have : document.querySelector(".money_have"),
+        money_insert : document.querySelector(".money_insert"),
+        button_clicked : document.querySelector(".button_clicked"),
+        status_log : document.querySelector(".status_log"),
+        refresh_wallet : document.querySelector(".refreshWallet"),
+        select_button : document.querySelector(".select_button")
+    }
+
     var item_panel = {
         li : document.querySelectorAll(".item_panel li"),
         highLighting :  function(){ //highLighting()
             var list = document.querySelectorAll(".item_panel li");
-            var money_insert = parseInt(document.querySelector(".money_insert").value);
+            // var money_insert = parseInt(document.querySelector(".money_insert").value);
             
             list.forEach(function(value){
                 var cost = parseInt(value.children[1].children[1].innerText);
-                if (money_insert >= cost) {
+                if (elements.money_insert.value >= cost) {
                     value.classList.add("yellow");
                 }
                 else value.classList.remove("yellow");
@@ -38,46 +48,47 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     var select_panel = {
         timer : null,
-        button_clicked  : document.querySelector(".button_clicked"),
-        money_insert : document.querySelector(".money_insert"),
+        //button_clicked  : document.querySelector(".button_clicked"),
+        //money_insert : document.querySelector(".money_insert"),
         run : function(){
             this.setSelectNumber();
         },
         setSelectNumber : function(){ 
-            document.querySelector(".select_button").addEventListener("click",function(e){
+            //document.querySelector(".select_button").addEventListener("click",function(e){
+            elements.select_button.addEventListener("click",function(e){
                 if(e.target && e.target.nodeName == "BUTTON"){
-                    this.button_clicked.value += e.target.innerText;
+                    elements.button_clicked.value += e.target.innerText;
                     clearTimeout(this.timer);
-                    this.timer = setTimeout(this.executeSelection.bind(this,this.button_clicked.value),3000);
+                    this.timer = setTimeout(this.executeSelection.bind(this,elements.button_clicked.value),3000);
                 }
             }.bind(this)); //timer에 대한 접근을 위해 bind
         },
          statusLogChanger : function(arg){ 
-            var statusLog = document.querySelector(".status_log");
-            statusLog.value += arg;
+            // var statusLog = document.querySelector(".status_log");
+            elements.status_log.value += arg;
             
-            if (statusLog.selectionStart == statusLog.selectionEnd) {
-                statusLog.scrollTop = statusLog.scrollHeight;
+            if (elements.status_log.selectionStart == elements.status_log.selectionEnd) {
+                elements.status_log.scrollTop = elements.status_log.scrollHeight;
             }
         },
 
         executeSelection : function(index){
             //console.log(item_panel.buyable(this.money_insert.value,index)); //없는번호 , 잔액부족 , 구매가능
-            var money_insert = this.money_insert;
-            var money_have = money_panel.money_have;
+            //var money_insert = this.money_insert;
+            //var money_have = money_panel.money_have;
             var flag;
-            index && (flag = item_panel.buyable(money_insert.value,index));
+            index && (flag = item_panel.buyable(elements.money_insert.value,index));
 
-            if(this.button_clicked.value == ""){//반환
-                this.statusLogChanger(money_insert.value + "원 반환\n");
-                money_have.value = parseInt(money_have.value)+ parseInt(money_insert.value);
-                money_insert.value = "0";
+            if(elements.button_clicked.value == ""){//반환
+                this.statusLogChanger(elements.money_insert.value + "원 반환\n");
+                elements.money_have.value = parseInt(elements.money_have.value)+ parseInt(elements.money_insert.value);
+                elements.money_insert.value = "0";
                 item_panel.highLighting();
                 money_panel.refreshWallet();
             }
             else if(flag == "구매가능"){//구매
-                this.money_insert.value -= item_list[index-1].cost;
-                this.button_clicked.value = "";
+                elements.money_insert.value -= item_list[index-1].cost;
+                elements.button_clicked.value = "";
                 this.statusLogChanger(item_list[index-1].name + "선택됨\n");
                 item_panel.highLighting();
 
@@ -86,7 +97,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
             }
             else {//없는번호 , 잔액부족
                 this.statusLogChanger(flag+"\n");
-                document.querySelector(".button_clicked").value = "";
+                //document.querySelector(".button_clicked").value = "";
+                elements.button_clicked.value = "";
                 
                 clearTimeout(this.timer);
                 this.timer = setTimeout(this.executeSelection.bind(this), 3000);
@@ -97,17 +109,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 
     var money_panel = {
-        money_button : document.querySelector(".money_button").children,
-        money_have : document.querySelector(".money_have"),
+        //money_button : document.querySelector(".money_button").children,
+        //money_have : document.querySelector(".money_have"),
         run : function() {
             this.refreshWallet();
             this.clickMoneyButton();
-            document.querySelector(".refreshWallet").addEventListener("click",this.refreshWallet.bind(this));
+            //document.querySelector(".refreshWallet").addEventListener("click",this.refreshWallet.bind(this));
+            elements.refresh_wallet.addEventListener("click",this.refreshWallet.bind(this));
         },
         clickMoneyButton : function(){
-            var money_insert = document.querySelector(".money_insert");
+            //var money_insert = document.querySelector(".money_insert");
     
-            document.querySelector(".money_button").addEventListener("click",function(e){
+            //document.querySelector(".money_button").addEventListener("click",function(e){
+            elements.money_button.addEventListener("click",function(e){
                 if (e.target && e.target.nodeName == "BUTTON"){
                     select_panel.statusLogChanger(e.target.innerText+"이 투입됨\n");
                     setHaveMoney(-e.target.parentNode.dataset.value);
@@ -118,8 +132,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
             }.bind(this)); //refreshWallet에 대한 접근을 위한 bind
         },
         refreshWallet : function(){
-            var money_button = document.querySelector(".money_button").children;
-            var temp = this.money_have.value;
+            //var money_button = document.querySelector(".money_button").children;
+            var money_button = elements.money_button.children;
+            var temp = elements.money_have.value;
             for(var i = money_button.length-1;i>=0;i--){
                 var container = money_button[i];
                 var button = container.children[0];
