50f534fe35021934045bf5c3d9efdba54ce82e58
DESKTOP-MVLSK2J\Tim <gwanhyung787@gmail.com>
2017-12-01 13:34:52 +0900

    Split controller & view

---
+++ js/controller.js
@@ -0,0 +1,159 @@
+(function (window) {
+	'use strict';
+
+	/**
+	 * Takes a store and view and acts as the controller between them
+	 *
+	 * @constructor
+	 * @param {object} store The store instance
+	 * @param {object} view The view instance
+	 */
+	function Controller(store, view) {
+		this.store = store;
+		this.view = view;
+		this.data = this.store.findAll();
+		this.nextEvent = null;
+		this.indicatorMoney = 0;
+		this.totalMoney = this.store.getTotalMoney();
+		this.id = '';
+
+		this.view.bind('withdrawMoney', (amount, moneyType) => {
+			this.withdrawMoney(amount, moneyType);
+		});
+
+		this.view.bind('inputProductId', (num) => {
+			this.inputProductId(num);
+		});
+	}
+
+	Controller.prototype.setView = function () {
+		this._updateState();
+	};
+
+	Controller.prototype.inputProductId = function (num) {
+		this.clearEvent();
+
+		if (this.indicatorMoney <= 0) {
+			this.view.render('writeLog', '돈을 투입하십시오.');
+			return;
+		}
+
+		this.id += num;
+		this.view.render('writeLog', this.id);
+
+		this.nextEvent = setTimeout(() => {
+			this.itemSelected();
+			this.id = '';
+		}, 3000);
+	}
+
+	Controller.prototype.itemSelected = function () {
+		this.clearEvent();
+
+		const item = this.data.products.filter(({
+			id
+		}) => id == this.id)[0];
+
+		if (!item) {
+			this.view.render('writeLog', '해당 상품이 존재하지 않음.');
+			return;
+		}
+
+		if (item.price > this.indicatorMoney) {
+			this.view.render('writeLog', '투입 금액이 ' + item.name + '의 가격보다 적음.');
+			return;
+		}
+
+		this.indicatorMoney -= item.price;
+
+		this.view.render('renderIndicatorMoney', this.indicatorMoney);
+		this.view.render('writeLog', item.id + '번 ' + item.name + '가 선택됨');
+		this.view.render('renderPurchasableProducts', this.indicatorMoney);
+
+		this.nextEvent = setTimeout(() => {
+			this.returnChanges();
+		}, 5000);
+	}
+
+
+	Controller.prototype.withdrawMoney = function ($amount, moneyType) {
+		this.clearEvent();
+
+		if (this.data.wallet[moneyType] <= 0) {
+			this.view.render('writeLog', moneyType + '이 없음.');
+			return this;
+		}
+		this.view.render('writeLog', moneyType + '이 투입됐음.');
+
+		this.calculateMoney(moneyType);
+		this.view.render('renderPurchasableProducts', this.indicatorMoney);
+		this.view.render('renderMoney', {
+			$amount,
+			moneyAmount: this.data.wallet[moneyType],
+			totalMoney: this.totalMoney,
+			indicatorMoney: this.indicatorMoney
+		});
+
+
+		this.nextEvent = setTimeout(() => {
+			this.returnChanges();
+		}, 5000);
+	};
+
+
+	Controller.prototype.returnChanges = function () {
+		this.clearEvent();
+
+		let changes = this.indicatorMoney;
+		this.indicatorMoney = 0;
+		this.totalMoney += changes;
+
+		this.view.render('writeLog', changes + '원을 반환함.');
+		this.view.render('renderPurchasableProducts', this.indicatorMoney);
+
+		Object.keys(this.data.wallet).reverse().forEach((moneyType) => {
+			const moneyTypeNum = +moneyType.slice(0, -1);
+			const addAmount = Math.floor(changes / moneyTypeNum);
+
+			if (addAmount) {
+				this.data.wallet[moneyType] += addAmount;
+				changes -= moneyTypeNum * addAmount;
+			}
+		})
+
+		this.view.render('renderWallet', {
+			wallet: this.data.wallet,
+			totalMoney: this.totalMoney,
+			indicatorMoney: this.indicatorMoney
+		});
+
+	};
+
+	Controller.prototype.clearEvent = function () {
+		if (this.nextEvent !== null) {
+			clearTimeout(this.nextEvent);
+			this.nextEvent = null;
+		}
+	};
+
+	Controller.prototype.calculateMoney = function (moneyType) {
+		const moneyTypeNum = +moneyType.slice(0, -1);
+		this.data.wallet[moneyType]--;
+		this.indicatorMoney += moneyTypeNum;
+		this.totalMoney -= moneyTypeNum;
+	};
+
+
+	Controller.prototype._updateState = function () {
+		this.view.render('showProducts', this.data.products);
+		this.view.render('renderWallet', {
+			wallet: this.data.wallet,
+			totalMoney: this.totalMoney,
+			indicatorMoney: this.indicatorMoney
+		});
+	};
+
+	// Export to window
+	window.app = window.app || {};
+	window.app.Controller = Controller;
+})(window);
---
+++ js/view.js
@@ -0,0 +1,114 @@
+/*global qs, qsa, $on, $parent, $delegate */
+
+(function (window) {
+	'use strict';
+
+	function View(template) {
+		this.template = template;
+
+		this.$productList = qs('.product-list');
+		this.$console = qs('.console');
+		this.$totalMoney = qs('.totalMoney');
+		this.$indicator = qs('.amount-indicator');
+		this.$moneyList = qs('.money-list');
+		this.$buttonList = qs('.button-list');
+		this.$money = qsa('.money-list > li');
+	}
+
+	View.prototype.bind = function (event, handler) {
+		if (event === 'withdrawMoney') {
+			$delegate(this.$moneyList, '.money .button', 'click', function () {
+				handler(this.parentNode.nextElementSibling, this.textContent);
+			});
+
+		} else if (event === 'inputProductId') {
+			$delegate(this.$buttonList, '.button-list .button', 'click', function () {
+				handler(this.textContent);
+			});
+		}
+	};
+
+	View.prototype.render = function (viewCmd, parameter) {
+		var viewCommands = {
+			showProducts: () => {
+				this.$productList.innerHTML = this.template.show(parameter);
+			},
+			renderWallet: () => {
+				this._renderWallet(parameter);
+			},
+			writeLog: () => {
+				this._writeLog(parameter);
+			},
+			renderPurchasableProducts: () => {
+				this._renderPurchasableProducts(parameter);
+			},
+			renderMoney: () => {
+				this._renderMoney(parameter);
+			},
+			renderIndicatorMoney: () => {
+				this._renderIndicatorMoney(parameter);
+			}
+		};
+
+		viewCommands[viewCmd]();
+	};
+
+	View.prototype._writeLog = function (text) {
+		const line = document.createElement('p');
+		line.innerHTML = text;
+		this.$console.appendChild(line);
+		this.$console.scrollTop = this.$console.offsetHeight;
+	};
+
+	View.prototype._renderWallet = function ({
+		wallet,
+		totalMoney,
+		indicatorMoney
+	}) {
+		Array.from(this.$money).forEach((element) => {
+			const moneyType = element.getAttribute('data-money');
+			this._renderMoneyAmount(element.children[1], wallet[moneyType]);
+		});
+		this._renderIndicatorMoney(indicatorMoney);
+		this._renderTotalMoney(totalMoney);
+	};
+
+	View.prototype._renderMoney = function ({
+		$amount,
+		moneyAmount,
+		indicatorMoney,
+		totalMoney
+	}) {
+		this._renderMoneyAmount($amount, moneyAmount);
+		this._renderIndicatorMoney(indicatorMoney);
+		this._renderTotalMoney(totalMoney);
+	};
+
+
+	View.prototype._renderMoneyAmount = function ($amount, moneyAmount) {
+		$amount.textContent = moneyAmount + '개';
+	}
+
+	View.prototype._renderTotalMoney = function (totalMoney) {
+		this.$totalMoney.textContent = totalMoney + '원';
+	}
+
+	View.prototype._renderIndicatorMoney = function (indicatorMoney) {
+		this.$indicator.textContent = indicatorMoney + '원';
+	}
+
+	View.prototype._renderPurchasableProducts = function (indicatorMoney) {
+		const $productPrice = qsa('.product-price');
+		Array.from($productPrice).forEach(function (el) {
+			if (el.textContent <= indicatorMoney) {
+				el.parentNode.previousSibling.style.backgroundColor = "yellow";
+			} else {
+				el.parentNode.previousSibling.style.backgroundColor = "lightblue";
+			}
+		});
+	};
+
+	// Export to window
+	window.app = window.app || {};
+	window.app.View = View;
+}(window));
