feaecfb635868b8916da9e4a43f2e29333e8e15c
Sehwan Kim <skim451@illinois.edu>
2018-01-03 14:05:59 +0900

    added new eventListener for wallet buttons.

--- vending_machine.js
+++ vending_machine.js
@@ -5,45 +5,7 @@ let moneyButtonsContainer = document.querySelector(".money_buttons")
 let moneyCountsContainer = document.querySelector(".money_counts")
 let monitorInsertedAmount = document.querySelector(".monitor_inserted_amount");
 let walletTotalAmount = document.querySelector(".total_amount");
-
-function initWallet() {
-	for(let i = 0; i < moneyTypes.length; i++) {
-	    let button = document.createElement("button")
-	    let moneyCount = document.createElement("span")
-	    button.textContent = moneyTypes[i]
-	    button.addEventListener("click", walletButtonEventListedner(button));
-	    moneyCount.textContent = "1 개"
-	    moneyCount.id = moneyTypes[i] + "type";
-	    moneyButtonsContainer.appendChild(button)
-	    moneyCountsContainer.appendChild(moneyCount)
-	}
-    walletTotalAmount.textContent = getWalletTotalAmount() + " 원"
-}
-
-function walletButtonEventListedner(button) {
-    let amount = parseInt(button.innerHTML) + parseInt(monitorInsertedAmount.textContent);
-    monitorInsertedAmount.textContent = amount + " 원";
-    // console.log(amount);
-    // console.log(monitorInsertedAmount.textContent);
-
-    // 카운트 뺴는거 해야됨
-    // console.log(button);
-    // let money = button.textContent
-    // console.log(money+"type");
-    // console.log(document.getElementById(money + "type"));
-    // console.log(document.querySelector("#10type"));
-}
-
-function getWalletTotalAmount() {
-	let moneyCounts = document.querySelector(".money_counts").childNodes
-    let totalAmount = 0
-    for(let i = 0; i < moneyCounts.length; i++) {
-        let moneyCount = parseInt(moneyCounts[i].textContent)
-        let moneyType = parseInt(moneyCounts[i].id)
-        totalAmount += moneyType * moneyCount
-    }
-    return totalAmount
-}
+let moneyCounts = document.querySelector(".money_counts").childNodes
 
 function initMenu() {
 	for(let i = 0; i < menu.length; i++) {
@@ -65,5 +27,45 @@ function initMenu() {
 	}
 }
 
-initWallet();
+function initWallet() {
+	for(let i = 0; i < moneyTypes.length; i++) {
+	    let button = document.createElement("button")
+	    let moneyCount = document.createElement("span")
+	    button.textContent = moneyTypes[i]
+	    button.addEventListener("click", function() {
+	    	let thisType = parseInt(this.innerHTML)
+	    	let thisCountContainer = document.getElementById(thisType + "type")
+	    	let thisCount = parseInt(thisCountContainer.textContent)
+
+	    	if(thisCount > 0) {
+	    		let currInsertedAmount = parseInt(monitorInsertedAmount.textContent)
+		    	let newInsertedAmount = thisType + currInsertedAmount
+				monitorInsertedAmount.textContent = newInsertedAmount + " 원";
+
+				let currWalletAmount = parseInt(walletTotalAmount.textContent)
+				let newWalletAmount = currWalletAmount - thisType 
+				walletTotalAmount.textContent = newWalletAmount 
+
+				thisCountContainer.textContent = (thisCount - 1) + " 개"
+	    	}	    	
+	    });
+	    moneyCount.textContent = "1 개"
+	    moneyCount.id = moneyTypes[i] + "type";
+	    moneyButtonsContainer.appendChild(button)
+	    moneyCountsContainer.appendChild(moneyCount)
+	}
+    walletTotalAmount.textContent = getWalletTotalAmount() + " 원"
+}
+
+function getWalletTotalAmount() {
+    let totalAmount = 0
+    for(let i = 0; i < moneyCounts.length; i++) {
+        let moneyCount = parseInt(moneyCounts[i].textContent)
+        let moneyType = parseInt(moneyCounts[i].id)
+        totalAmount += moneyType * moneyCount
+    }
+    return totalAmount
+}
+
 initMenu();
+initWallet();
