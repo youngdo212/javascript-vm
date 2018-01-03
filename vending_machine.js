67df99c0d7d857938128a5e950130b358e1208cb
Sehwan Kim <skim451@illinois.edu>
2018-01-03 11:51:02 +0900

    switch coder

--- vending_machine.js
+++ vending_machine.js
@@ -3,10 +3,11 @@ let money_types = data.money_types
 let menu_container = document.querySelector(".menu")
 let money_buttons_container = document.querySelector(".money_buttons")
 let money_counts_container = document.querySelector(".money_counts")
-let monitor_inserted_amount = document.querySelector(".monitor_inserted_amount"); 
+let monitor_inserted_amount = document.querySelector(".monitor_inserted_amount");
+let wallet_total_amount = document.querySelector(".total_amount");
 
 function init_wallet() {
-	for(i in money_types) {
+	for(let i = 0; i < money_types.length; i++) {
 	    let button = document.createElement("button")
 	    let money_count = document.createElement("span")
 	    button.textContent = money_types[i]
@@ -16,9 +17,16 @@ function init_wallet() {
 	    	// 카운트 뺴는거 해야됨 
 	    });
 	    money_count.textContent = "0 개"
+	    money_count.id = "type" + money_types[i];
 	    money_buttons_container.appendChild(button)
 	    money_counts_container.appendChild(money_count)
 	}	
+
+	let money_counts = document.getElementsByClassName("money_counts");
+	money_counts = money_counts[0].childElement
+	for(i in money_counts) {
+		console.log(money_counts[i])
+	}
 }
 
 function init_menu() {
@@ -34,8 +42,6 @@ function init_menu() {
 		namefield.textContent = item.name; 
 		pricefield.textContent = (i + 1) + ". " + item.price; 
 
-		console.log(item.name); 
-		console.log(item.price); 
 		item_container.appendChild(namefield); 
 		item_container.appendChild(pricefield); 
 
