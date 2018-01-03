5c57739931fb50d7503365e6d6784ae8cfb11432
HannaJeon <jks7psj@gmail.com>
2018-01-03 10:59:24 +0900

    wallet container 동적으로 데이터 삽입

--- vending_machine.js
+++ vending_machine.js
@@ -1,4 +1,12 @@
-// var data = JSON.parse(data);
-// console.log(data);
-//
-// var wallet = document.getElementsByClassName("wallet")
+let money_types = data.money_types
+let money_buttons_container = document.querySelector(".money_buttons")
+let money_counts_container = document.querySelector(".money_counts")
+
+for(i in money_types) {
+    let button = document.createElement("button")
+    let money_count = document.createElement("span")
+    button.textContent = money_types[i]
+    money_count.textContent = "0 개"
+    money_buttons_container.appendChild(button)
+    money_counts_container.appendChild(money_count)
+}
