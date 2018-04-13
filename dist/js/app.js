060da9e89cf21d5824ad61fdb8d722eae7c75642
Dongwoo Lee <likedemian@me.com>
2018-04-13 16:41:48 +0900

    Fix: variables names and refactor

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,20 +1,22 @@
-const coinStatus = document.querySelector('.selector__status__coin');
-const slot = document.querySelector('.coin-slot__lists');
-const beverages = document.querySelector('.beverage__lists');
-const selectButtonsLists = document.querySelector('.selector__buttons__lists');
-const buttonNums = selectButtonsLists.querySelectorAll('.selector__buttons__items');
+let selectButtonsLists = document.querySelector('.selector__buttons__lists');
 
-const statusScreen = document.querySelector('.selector__status__items');
-let selectDecision = ''
+const buttonLists = {
+  buttonNums: selectButtonsLists.querySelectorAll('.selector__buttons__items'),
+  buttonZero: document.querySelector('#selector__button__0'),
+  buttonConfirm: document.querySelector('#selector__button__confirm'),
+}
 
-
-
+let coinStatus = document.querySelector('.selector__status__coin');
+let slot = document.querySelector('.coin-slot__lists');
+let beverages = document.querySelector('.beverage__lists');
+let statusScreen = document.querySelector('.selector__status__items');
+let selectDecision = '';
 
 statusScreen.innerHTML = "동전을 넣어주세요"
 
 
 const renders = {
-  coinRender(val) {
+  renderCoin(val) {
     let coinOutput = '';
     val.forEach(elem => {
       coinOutput += `
@@ -30,9 +32,8 @@ const renders = {
     slot.innerHTML += coinOutput;
   },
 
-  bevRender(val) {
+  renderBeverage(val) {
     let bevOutput = '';
-
     val.forEach((elem) => {
       bevOutput += `
       <li class="beverage__items">
@@ -46,28 +47,40 @@ const renders = {
     });
     return beverages.innerHTML += bevOutput;
   }
+
 }
 
-renders.coinRender(coin);
-renders.bevRender(bevLists);
+
+renders.renderCoin(coin);
+renders.renderBeverage(bevLists);
 
 
-const buttonZero = document.querySelector('#selector__button__0');
-const buttonConfirm = document.querySelector('#selector__button__confirm');
+function activateBtn() {
+  Number(coinStatus.innerHTML) >= 300 ? buttonLists.buttonConfirm.disabled = false : buttonLists.buttonConfirm.disabled = true;
+}
 
-const buttons = {
+
+function availableItems() {
+  let bevItems = beverages.querySelectorAll('.beverage__items');
+  for (let i = 0; i < bevItems.length; i++) {
+    Number(coinStatus.innerHTML) < bevLists[i].price ? bevItems[i].style.backgroundColor = '' : bevItems[i].style.backgroundColor = '#ffafaf';
+  }
+}
+
+
+const selections = {
   selectNums(val) {
     val.forEach(elem => {
       elem.addEventListener('click', (event) => {
         selectDecision += event.target.innerHTML;
-        statusScreen.innerHTML = `입력 변호: ${selectDecision}`
+        statusScreen.innerHTML = `입력 변호: ${selectDecision}`;
       })
     });
   },
-
+  
   selectZero(val) {
     val.addEventListener('click', (event) => {
-      selectDecision += '0'
+      selectDecision += '0';
     });
   },
 
@@ -79,68 +92,48 @@ const buttons = {
       availableItems();
     });
   }
+
 }
 
-buttons.selectNums(buttonNums);
-buttons.selectZero(buttonZero);
-buttons.selectConfirm(buttonConfirm);
 
+function insertCoin() {
+  let coinButtons = slot.querySelectorAll('.coin-slot__buttons');
+  let stores = slot.querySelectorAll('.coin__left');
 
+  for (let i = 0; i < coinButtons.length; i++) {
+    coinButtons[i].addEventListener('click', (event) => {     
+      coin[i].store--;
+      if (coin[i].store <= 0) {
+        coinButtons[i].disabled = true;
+        coinButtons[i].style.backgroundColor = '#cfcfcf'
+      }
 
-
-
-const availableItems = () => {
-  for (let i = 0; i < bevItems.length; i++) {
-    Number(coinStatus.innerHTML) < bevLists[i].price ? bevItems[i].style.backgroundColor = '' : bevItems[i].style.backgroundColor = '#ffafaf';
+      sumCoin -= coin[i].value;
+      inputCoin += coin[i].value;
+      totalCoin.innerHTML = `₩ ${sumCoin}원`;
+      coinStatus.innerHTML = inputCoin;
+      stores[i].innerHTML = `${coin[i].store}개`;
+      statusScreen.innerHTML = `원하는 음료의 번호를 입력하세요`;
+      
+      activateBtn();
+      availableItems();
+    })
   }
 }
 
-const activateBtn = () => {
-  Number(coinStatus.innerHTML) >= 300 ? buttonConfirm.disabled = false : buttonConfirm.disabled = true;
-}
+insertCoin();
 
 
 
 
-let coinButtons = slot.querySelectorAll('.coin-slot__buttons');
-let stores = slot.querySelectorAll('.coin__left');
-let bevItems = beverages.querySelectorAll('.beverage__items');
 
 
 
-
-for (let i = 0; i < coinButtons.length; i++) {
-  coinButtons[i].addEventListener('click', (event) => {
-    coin[i].store--;
-
-    if (coin[i].store <= 0) {
-      coinButtons[i].disabled = true;
-      coinButtons[i].style.backgroundColor = '#cfcfcf'
-    }
-
-    sumCoin -= coin[i].value;
-    inputCoin += coin[i].value;
-
-    totalCoin.innerHTML = `₩ ${sumCoin}원`;
-    coinStatus.innerHTML = inputCoin;
-    stores[i].innerHTML = `${coin[i].store}개`;
-    statusScreen.innerHTML = `원하는 음료의 번호를 입력하세요`;
-
-    activateBtn();
-    availableItems();
-  })
-}
-
-
-
-
-// const selectReturn = document.querySelector('#selector__button__return');
 
 function purchaseItem() {
   bevLists.forEach(elem => {
     if (elem.id === selectDecision && elem.price <= Number(coinStatus.innerHTML)) {
       coinStatus.innerHTML -= elem.price;
-      coinStatus.innerHTML = Number(coinStatus.innerHTML);
       inputCoin = Number(coinStatus.innerHTML);
       statusScreen.innerHTML = `${elem.name}을 선택했습니다`;
       selectDecision = '';
@@ -150,3 +143,8 @@ function purchaseItem() {
     }
   });
 }
+
+
+selections.selectNums(buttonLists.buttonNums);
+selections.selectZero(buttonLists.buttonZero);
+selections.selectConfirm(buttonLists.buttonConfirm);
