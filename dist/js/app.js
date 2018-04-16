67441cb7fa290fd7d72b8d2c1fa70e2c9f97b389
Dongwoo Lee <likedemian@me.com>
2018-04-16 20:30:04 +0900

    :+1: update: put monitor class into the control class

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,12 +1,3 @@
-let selectButtonsLists = document.querySelector('.selector__buttons__lists');
-const buttonLists = {
-  buttonNums: selectButtonsLists.querySelectorAll('.selector__buttons__items'),
-  buttonZero: document.querySelector('#selector__button__0'),
-  buttonConfirm: document.querySelector('#selector__button__confirm'),
-}
-let selectDecision = '';
-
-
 class Renders {
   constructor() {
     this.coinOutput = '';
@@ -14,140 +5,157 @@ class Renders {
     this.beverages = document.querySelector('.beverage__lists');
     this.slot = document.querySelector('.coin-slot__lists');
   }
+
+
   renderCoin(val) {
-    val.forEach(elem => {
-      this.coinOutput += `
+    this.coinOutput = val.reduce((acc, curr) => {
+      sumCoin += curr.value * curr.store;
+      return acc += `
       <li>
-        <button class="coin-slot__buttons">${elem.name}</button>
-          <p class="coin__left">${elem.store}개
+        <button class="coin-slot__buttons">${curr.name}</button>
+          <p class="coin__left">${curr.store}개
         </button>
       </li>
       `
-      sumCoin += elem.value * elem.store;
-    })
+    }, '')
     totalCoin.innerHTML = `₩ ${sumCoin}원`
     this.slot.innerHTML += this.coinOutput;
-  }
+  };
 
   renderBeverage(val) {
-    val.forEach((elem) => {
-      this.bevOutput += `
+    this.bevOutput = val.reduce((acc, curr) => {
+      return acc += `
       <li class="beverage__items">
-        <p class="beverage__name">${elem.name}</p>
+        <p class="beverage__name">${curr.name}</p>
         <p class="beverage__price">
-          <span class="beverage__id">${elem.id}. </span>
-          ${elem.price}원
+          <span class="beverage__id">${curr.id}. </span>
+          ${curr.price}원
         </p>
       </li>
       `;
-    });
+    }, '');
     return this.beverages.innerHTML += this.bevOutput;
-  }
+  };
 }
+
+
+const render = new Renders();
+render.renderCoin(coin);
+render.renderBeverage(bevLists);
+
+
 
 
 
 class Monitors {
   constructor() {
+    this.selectDecision = '';
     this.bevItems = render.beverages.querySelectorAll('.beverage__items');
-    this.coinStatus = document.querySelector('.selector__status__coin');
     this.statusScreen = document.querySelector('.selector__status__items');
-  }
+    this.coinStatus = document.querySelector('.selector__status__coin');
+  };
 
   activateBtn() {
-    if (Number(this.coinStatus.innerHTML) >= 300) {
-      buttonLists.buttonConfirm.disabled = false
-      this.statusScreen.innerHTML = "음료를 골라주세요"
-    } else {
-      buttonLists.buttonConfirm.disabled = true;
-    }
-  }
+    Number(this.coinStatus.innerHTML) >= 300 ?
+      control.buttonLists.buttonConfirm.disabled = false :
+      control.buttonLists.buttonConfirm.disabled = true;
+  };
 
   availableItems() {
     for (let i = 0; i < this.bevItems.length; i++) {
       Number(this.coinStatus.innerHTML) < bevLists[i].price ?
-        this.bevItems[i].style.backgroundColor = '' :
-        this.bevItems[i].style.backgroundColor = '#ffafaf';
+        this.bevItems[i].style.backgroundImage = '' :
+        this.bevItems[i].style.backgroundImage = 'linear-gradient(to top, #e6b980 0%, #eacda3 100%)';
     }
   }
 
   purchase() {
     bevLists.forEach(elem => {
-      if (elem.id === selectDecision && elem.price <= Number(this.coinStatus.innerHTML)) {
+      if (elem.id === this.selectDecision && elem.price <= Number(this.coinStatus.innerHTML) && elem.working) {
         this.coinStatus.innerHTML -= elem.price;
         inputCoin = Number(this.coinStatus.innerHTML);
         this.statusScreen.innerHTML = `${elem.name}을 선택했습니다`;
-        selectDecision = '';
+        this.selectDecision = '';
         this.activateBtn();
-      } else if (elem.id === selectDecision && elem.price > Number(this.coinStatus.innerHTML)) {
+      } else if (elem.id === this.selectDecision && elem.price > Number(this.coinStatus.innerHTML) && elem.working) {
         this.statusScreen.innerHTML = "동전을 더 넣어주세요";
+        control.monitor.selectDecision = '';
       }
     });
   }
 }
 
 
-class Selections {
+const monitor = new Monitors();
+
+
+
+
+class Controls {
   constructor() {
+    this.selectButtonsLists = document.querySelector('.selector__buttons__lists');
+    this.buttonLists = {
+      buttonNums: this.selectButtonsLists.querySelectorAll('.selector__buttons__items'),
+      buttonZero: document.querySelector('#selector__button__0'),
+      buttonConfirm: document.querySelector('#selector__button__confirm'),
+    };
     this.coinButtons = render.slot.querySelectorAll('.coin-slot__buttons');
     this.stores = render.slot.querySelectorAll('.coin__left');
+    this.monitor = monitor;
   }
-  
+
+  insertCoin() {
+    for (let i = 0; i < this.coinButtons.length; i++) {
+      this.coinButtons[i].addEventListener('click', (event) => {
+        this.monitor.selectDecision = '';
+        coin[i].store--;
+        if (coin[i].store <= 0) {
+          this.coinButtons[i].disabled = true;
+          this.coinButtons[i].style.backgroundColor = '#cfcfcf'
+        }
+        sumCoin -= coin[i].value;
+        inputCoin += coin[i].value;
+        totalCoin.innerHTML = `₩ ${sumCoin}원`;
+        this.monitor.coinStatus.innerHTML = inputCoin;
+        this.stores[i].innerHTML = `${coin[i].store}개`;
+        this.monitor.activateBtn();
+        this.monitor.availableItems();
+        this.monitor.statusScreen.innerHTML = `원하는 음료의 번호를 입력하세요`;
+      })
+    }
+  }
+
   selectNums(val) {
     val.forEach(elem => {
       elem.addEventListener('click', (event) => {
-        selectDecision += event.target.innerHTML;
-        monitor.statusScreen.innerHTML = `입력 변호: ${selectDecision}`;
+        this.monitor.selectDecision += event.target.innerHTML;
+        this.monitor.statusScreen.innerHTML = `입력 변호: ${this.monitor.selectDecision}`;
       })
     });
   }
 
   selectZero(val) {
     val.addEventListener('click', (event) => {
-      selectDecision += '0';
+      this.monitor.selectDecision += '0';
     });
   }
 
   selectConfirm(val) {
-    val.addEventListener('click', function (event) {
-      selectDecision = Number(selectDecision);
-      inputCoin = Number(monitor.coinStatus.innerHTML);
-      monitor.purchase()
-      monitor.availableItems();
+    val.addEventListener('click', (event) => {
+      this.monitor.selectDecision = Number(this.monitor.selectDecision);
+      inputCoin = Number(this.monitor.coinStatus.innerHTML);
+      this.monitor.purchase()
+      this.monitor.availableItems();
     });
-  }
-
-  insertCoin() {
-    for (let i = 0; i < this.coinButtons.length; i++) {
-      this.coinButtons[i].addEventListener('click', (event) => {
-        coin[i].store--;
-        if (coin[i].store <= 0) {
-          this.coinButtons[i].disabled = true;
-          this.coinButtons[i].style.backgroundColor = '#cfcfcf'
-        }
-
-        sumCoin -= coin[i].value;
-        inputCoin += coin[i].value;
-        totalCoin.innerHTML = `₩ ${sumCoin}원`;
-        monitor.coinStatus.innerHTML = inputCoin;
-        this.stores[i].innerHTML = `${coin[i].store}개`;
-        monitor.statusScreen.innerHTML = `원하는 음료의 번호를 입력하세요`;
-
-        monitor.activateBtn();
-        monitor.availableItems();
-      })
-    }
   }
 }
 
-const render = new Renders();
-render.renderCoin(coin);
-render.renderBeverage(bevLists);
 
-const selection = new Selections();
-selection.selectNums(buttonLists.buttonNums);
-selection.selectZero(buttonLists.buttonZero);
-selection.selectConfirm(buttonLists.buttonConfirm);
-selection.insertCoin();
+const control = new Controls();
 
-const monitor = new Monitors();
+
+control.insertCoin();
+control.selectNums(control.buttonLists.buttonNums);
+control.selectZero(control.buttonLists.buttonZero);
+control.selectConfirm(control.buttonLists.buttonConfirm);
+control.monitor.statusScreen.innerHTML = "동전을 넣어주세요"
