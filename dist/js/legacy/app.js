bc2fdbfefbe2e55d5913de08d3291a71a43f1041
Dongwoo Lee <likedemian@me.com>
2018-04-30 15:51:58 +0900

    del: old scripts files

--- dist/js/legacy/app.js
+++
@@ -1,7 +0,0 @@
-const vmTemplate = new VMTemplate();
-const vmModel = new VMModel();
-const vmView = new VMViewer();
-const vmControl = new VMController(vmView);
-
-vmTemplate.init();
-vmControl.init();
