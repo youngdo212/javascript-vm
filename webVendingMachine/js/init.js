150b9186a80e81d8b9eb59415aa08f0513e98133
chany <lyhy0310@gmail.com>
2018-05-07 21:28:25 +0900

    [add] init js - templeate add

---
+++ webVendingMachine/js/init.js
@@ -0,0 +1,151 @@
+// element-Search 
+const snackListElement = document.querySelector('.snack-list');
+const vendingMachineElement = document.querySelector('.vending-machine');
+const vendingMachineMoneyElement = vendingMachineElement.querySelector('.diplay-inserted-money .money');
+const vendingMachineButtonsElement = vendingMachineElement.querySelector('.number-buttons');
+const walletButtonListElement = document.querySelector('.money-button-list');
+const walletTotalMoneyElement = document.querySelector('.total-my-assets .money');
+// class  money ,wallet, vm ,
+class Money {
+  constructor(value){
+    this.value = value;
+    this.unit =  '원';
+  }
+}
+
+class MoneyList {
+  constructor(value, count){
+    this.moneyList = new Array(count).fill(new Money(value))
+  }
+}
+
+
+class Wallet {
+  constructor(moneyList){
+    this.moneyList=moneyList;
+  }
+  useMoney(money){
+    this.moneyList[money].moneyList.pop();
+  }
+  getTotalMoneyList(){
+    const totalMoneyList = Object.values(this.moneyList)
+    return totalMoneyList;
+  }
+  get totalMoney(){
+    let totalMoney = 0;
+    this.getTotalMoneyList().forEach(v=>{
+      const totalThisKind = v.moneyList.reduce((ac,c)=>{
+      return ac+=c.value
+    },0)
+    totalMoney+=totalThisKind;
+  });
+  return totalMoney;
+  }
+}
+
+class VendingMachine {
+  constructor(){
+    this.money=0;
+  }
+  insertMoney(money){
+    this.money += money
+  }
+}
+const vendingMachine = new VendingMachine();
+
+
+
+const myMoney = {
+  "100원":  new MoneyList(100,5), 
+  "1000원": new MoneyList(1000,5),
+  "5000원": new MoneyList(5000,2),
+  "10000원": new MoneyList(10000,2),
+}
+
+const wallet = new Wallet(myMoney);
+
+const snacksList = [
+  { "id": 1, "name": "콜라", "price": 500, "working": true },
+  { "id": 2, "name": "사이다", "price": 1000, "working": true },
+  { "id": 3, "name": "파인애플맛 환타", "price": 400, "working": true },
+  { "id": 4, "name": "포도맛 환타", "price": 300, "working": true },
+  { "id": 5, "name": "레몬에이드", "price": 900, "working": true },
+  { "id": 6, "name": "봉봉", "price": 1200, "working": true },
+  { "id": 7, "name": "코코아주스", "price": 1000, "working": true },
+  { "id": 8, "name": "콜라제로", "price": 1000, "working": true },
+  { "id": 9, "name": "파워에이드", "price": 2000, "working": true },
+  { "id": 10, "name": "초코우유", "price": 1000, "working": true },
+  { "id": 11, "name": "초코우유2", "price": 700, "working": true },
+  { "id": 12, "name": "초코우유3", "price": 600, "working": true },
+  { "id": 13, "name": "딸바우유", "price": 1000, "working": true },
+  { "id": 14, "name": "바나나우유", "price": 500, "working": true },
+  { "id": 15, "name": "커피우유", "price": 1000, "working": true },
+  { "id": 16, "name": "알로에", "price": 1200, "working": true },
+  { "id": 17, "name": "콘칩", "price": 1000, "working": true },
+  { "id": 18, "name": "새우깡", "price": 1000, "working": true },
+  { "id": 19, "name": "감자칩", "price": 2000, "working": true },
+  { "id": 20, "name": "칸쵸", "price": 1000, "working": true },
+  { "id": 21, "name": "아몬드", "price": 450, "working": true },
+  { "id": 22, "name": "다크초콜릿", "price": 1500, "working": true },
+  { "id": 23, "name": "가나초콜릿", "price": 1200, "working": true },
+  { "id": 24, "name": "견과류", "price": 900, "working": true },
+  { "id": 25, "name": "육포", "price": 1000, "working": true },
+  { "id": 26, "name": "오징어포", "price": 900, "working": true },
+  { "id": 27, "name": "미니땅콩", "price": 4000, "working": true },
+  { "id": 28, "name": "오징어", "price": 2300, "working": true },
+  { "id": 29, "name": "{고장}", "price": 1000, "working": false },
+  { "id": 30, "name": "신라면", "price": 700, "working": true },
+  { "id": 31, "name": "진라면", "price": 800, "working": true },
+  { "id": 32, "name": "포도맛 환타", "price": 1000, "working": true }
+]
+
+
+// template Lists
+
+const snacksHTMLTempleate = snacksList.reduce((ac,c)=>{
+  return ac+=`<li class="snack-list-item">
+                <div class="snack-name-container">
+                    <span class="snak-name">${c.name}</span>
+                </div>
+                <div class="label-price">
+                    <span class="snack-number">${c.id}</span>
+                    <span class="snack-price">${c.price}</span>
+                </div>
+              </li>`
+},'')
+
+const inserTedMoneyTemplate = `<p><span class="money">${vendingMachine.money}</span><span class="unit">원</span></p>`
+
+const vmButtonTextList = [1,2,3,4,5,6,7,8,9,0,"선택","취소"]
+
+const vendingMachineButtonTemplate = vmButtonTextList.reduce((ac,c)=>{
+  return ac+=` <li><button class="select-button">${c}</button></li>`
+}, '');
+
+console.dir(wallet.getTotalMoneyList()[0])
+console.dir(wallet.getTotalMoneyList()[0].moneyList)
+
+
+const walletButtonListTemplate = wallet.getTotalMoneyList().reduce((ac,c)=>{
+  const {length} = c.moneyList  
+  const {value, unit} =c.moneyList[0]
+  return ac+=`<li class="wallet-money-button">
+                <button class="money-button">${value}${unit}</button>
+                <span class="money-count">${length}개</span>
+              </li>`
+}, '');
+
+
+const templateRender = (element, template)=> {
+  element.insertAdjacentHTML('beforeend', template)
+}
+
+// redering Templeate
+document.addEventListener("DOMContentLoaded", (e)=> {
+  console.log("DOM fully loaded and parsed");
+  templateRender(snackListElement, snacksHTMLTempleate)
+  templateRender(vendingMachineButtonsElement, vendingMachineButtonTemplate)
+  templateRender(walletButtonListElement, walletButtonListTemplate)
+  walletTotalMoneyElement.innerText=wallet.totalMoney
+});
+
