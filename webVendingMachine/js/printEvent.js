670979f521e539b5ca13e746bc1cbaba8f9f0c34
chany <lyhy0310@gmail.com>
2018-05-07 15:12:58 +0900

    [modify] layout css and js class add

--- webVendingMachine/js/printEvent.js
+++ webVendingMachine/js/printEvent.js
@@ -1,14 +1,38 @@
-const walletButtonList = document.querySelectorAll('.wallet-money-buttonSet > button')
-console.log(walletButtonList)
-const map = (list, fn)=> {
-  const mappingList = [];
-  for(item of list){
-    mappingList.push(fn(item))
+class Wallet {
+  constructor(money){
+    this.money=money;
   }
-  return mappingList;
+  useMoney(money){
+    if(this.money>=money) this.money-=money;
+  }
 }
-map(walletButtonList, (button)=>{
-  button.addEventListener('click',(e)=>{
-    console.log(e.target.innerText)
-  })
+class VendingMachine {
+  constructor(){
+    this.money=0;
+  }
+  insertMoney(money){
+    this.money += money
+  }
+}
+const vendingMachine = new VendingMachine();
+
+const wallet = new Wallet(23500);
+const totalMoneyElement = document.querySelector('.total-my-assets .money')
+const vmMoneyElement = document.querySelector('.diplay-inserted-money .money')
+
+totalMoneyElement.innerText = `${wallet.money}`
+
+const walletButtons = document.querySelector('.money-button-list');
+walletButtons.addEventListener('click',(e)=>{
+    if(e.target.className!=="money-button") return;
+      const choseMoney = Number(e.target.innerText.split('원')[0])
+      if(wallet.money<choseMoney) return;
+        wallet.useMoney(choseMoney);
+        totalMoneyElement.innerText = `${wallet.money}`   
+        vendingMachine.insertMoney(choseMoney);
+        vmMoneyElement.innerText = `${vendingMachine.money}`
 })
+
+
+
+
