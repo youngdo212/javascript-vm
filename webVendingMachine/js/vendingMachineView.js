729792793498b3fd4520645085dfee6989ee3d2b
chany <lyhy0310@gmail.com>
2018-05-09 16:52:54 +0900

    [delete] printEvent and  init.js naming 수정

--- webVendingMachine/js/vendingMachineView.js
+++ webVendingMachine/js/vendingMachineView.js
@@ -160,7 +160,20 @@ document.addEventListener("DOMContentLoaded", (e)=> {
   viewUpdateWalletTotal();
 });
 
-
+rederingView.getSearched('.money-button-list').addEventListener('click',(e)=>{
+  if(e.target.localName!=="button") return;
+  const choseMoney =e.target.dataset.money
+  const moneyCountElement = e.target.nextElementSibling
+  let moneyCount = wallet.moneyKinds[choseMoney]
+  // 해당 돈의 종류가 있으면 
+  if(moneyCount){
+    const willUseMoney = wallet.useMoney(Number(choseMoney));
+    moneyCountElement.innerText=`${wallet.moneyKinds[choseMoney]}개`
+    vendingMachine.insertMoney(willUseMoney);
+    viewUpdateWalletTotal();
+    viewUpdateInsertedMoney();
+  }
+})
 
 
 
