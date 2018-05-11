e10d42b29b506b41e1bd9c636ac8fb82c51047df
Dongwoo Lee <likedemian@me.com>
2018-05-11 17:28:32 +0900

    add: feats to exports methods to other files

---
+++ dist/js/helpers.js
@@ -0,0 +1,14 @@
+const $qs = (selector, scope) => {
+  return (scope || document).querySelector(selector);
+};
+
+const $qsa = (selector, scope) => {
+  return (scope || document).querySelectorAll(selector);
+}
+
+const $on = (target, type, callback, capture) => {
+  target.addEventListener(type, callback, !!capture);
+}
+
+
+module.exports = {$qs, $qsa, $on}
