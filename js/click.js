a212011cd57a316a60f36ca45fcaf4aaa4e73448
youngdo212 <nsa06031@gmail.com>
2018-05-29 23:30:02 +0900

    update: 클래스화

--- js/click.js
+++ js/click.js
@@ -1,7 +1,16 @@
-const moneyButtonList = document.querySelectorAll('.money_button');
+class Button{
+  constructor(buttonList){
+    this.buttons = Array.from(buttonList)
+  }
+  addClickPrintEvent(){
+    this.buttons.forEach(button =>{
+      button.addEventListener('click', function(){
+        console.log(button.textContent);
+      });
+    })
+  }
+}
 
-Array.from(moneyButtonList).forEach(button=>{
-  button.addEventListener('click', function(){
-    console.log(button.textContent);
-  });
-})
+const moneyButtonList = document.querySelectorAll('.money_button');
+const buttons = new Button(moneyButtonList);
+buttons.addClickPrintEvent();
