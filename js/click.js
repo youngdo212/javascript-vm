21df515525824182ed8ea9db32913accd93e6f1f
youngdo212 <nsa06031@gmail.com>
2018-06-01 00:14:51 +0900

    feat: 지갑에서 돈 선택 가능
    vendingMachine 클래스 생성

--- js/click.js
+++ js/click.js
@@ -1,16 +1,40 @@
-class Button{
-  constructor(buttonList){
-    this.buttons = Array.from(buttonList)
-    this.addClickPrintEvent();
+class VendingMachine{
+  constructor(){
+    this.buttonList = document.querySelector("#money_button_list");
+    this.moneyNumberList = document.querySelector("#money_number_list").children;
+    this.walletTotalMoney = document.querySelector("#total_money_box>span");
+    this.inputMoney = document.querySelector("#input_money_box>span");
   }
-  addClickPrintEvent(){
-    this.buttons.forEach(button =>{
-      button.addEventListener('click', function(){
-        console.log(button.textContent);
-      });
-    })
+  init(){
+    this.buttonList.addEventListener("click", this.clickEvent.bind(this));
+  }
+  clickEvent(evt){
+    if(evt.target.tagName === "BUTTON"){
+      const index = evt.target.getAttribute("index");
+      const amount = evt.target.getAttribute("amount");
+      this.action(index, Number(amount));
+    }
+  }
+  action(index, amount){
+    if(this.moneyNumberList[index].firstElementChild.textContent > 0){
+      this.decreaseMoneyNumber(index);
+      this.decreaseWalletMoney(amount);
+      this.increaseInputMoney(amount);
+    }
+  }
+  decreaseMoneyNumber(index){
+    const target = this.moneyNumberList[index].firstElementChild;
+    target.textContent = Number(target.textContent) -1;
+  }
+  decreaseWalletMoney(amount){
+    const target = this.walletTotalMoney;
+    target.textContent = Number(target.textContent) - amount;
+  }
+  increaseInputMoney(amount){
+    const target = this.inputMoney;
+    target.textContent = Number(target.textContent) + amount;
   }
 }
 
-const moneyButtonList = document.querySelectorAll('.money_button');
-const buttons = new Button(moneyButtonList);
+let vendingMachine = new VendingMachine();
+vendingMachine.init();
