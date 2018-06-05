b7e6b05406be8212a4fe37c114c75deb8e203af1
youngdo212 <nsa06031@gmail.com>
2018-06-05 20:01:32 +0900

    fix: 0원 자판기에 들어가는 버그

--- js/vendingMachine.js
+++ js/vendingMachine.js
@@ -4,6 +4,7 @@ class VendingMachine{
     this.log = document.querySelector("#log");
   }
   inputMoney(money){
+    if(money == 0) return;
     this.totalMoney.textContent = Number(this.totalMoney.textContent) + money;
     this.logging(money + '원이 투입됐습니다');
   }
@@ -12,7 +13,7 @@ class VendingMachine{
     const text = document.createTextNode(content);
 
     if(this.log.children.length >= 10) this.log.removeChild(this.log.firstElementChild);
-        
+
     newLog.appendChild(text);
     this.log.appendChild(newLog);
   }
