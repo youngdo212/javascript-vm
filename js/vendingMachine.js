93a4b33d5c38eaae103251cd4f8f15bfc4f6606d
youngdo212 <nsa06031@gmail.com>
2018-06-05 19:56:49 +0900

    feat: 로깅 기능 추가

--- js/vendingMachine.js
+++ js/vendingMachine.js
@@ -1,8 +1,19 @@
 class VendingMachine{
   constructor(){
     this.totalMoney = document.querySelector("#vm_money_box>span");
+    this.log = document.querySelector("#log");
   }
   inputMoney(money){
     this.totalMoney.textContent = Number(this.totalMoney.textContent) + money;
+    this.logging(money + '원이 투입됐습니다');
+  }
+  logging(content){
+    const newLog = document.createElement("DIV");
+    const text = document.createTextNode(content);
+
+    if(this.log.children.length >= 10) this.log.removeChild(this.log.firstElementChild);
+        
+    newLog.appendChild(text);
+    this.log.appendChild(newLog);
   }
 }
