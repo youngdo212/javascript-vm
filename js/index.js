f108b7b3dae5fff80d3c6cfd06adc8380852907f
youngdo212 <nsa06031@gmail.com>
2018-06-22 21:51:59 +0900

    add: template 모듈 추가

--- js/index.js
+++ js/index.js
@@ -2,15 +2,7 @@ import {VendingMachine} from "./vendingMachine.js"
 import {Wallet} from "./wallet.js"
 import {Action} from "./Action.js"
 import {itemData} from "./itemData.js"
-
-function makeItemHtml({number, name, price}){
-  return `<li class="item" data-number=${number} data-price=${price}>
-  <dl>
-    <dt class='item_name'>${name}</dt>
-    <dd>${number}. ${price}</dd>
-  </dl>
-  </li>`
-}
+import {makeItemHtml} from "./template.js"
 
 const vm = new VendingMachine({
   vendingMachineWrap: document.querySelector('.vending_machine_wrap'),
